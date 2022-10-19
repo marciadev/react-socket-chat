5c8ab21158d1746c834df246517d00dacf72c64b
Marcia Leite <marcialeite483@gmail.com>
2022-10-19 19:01:37 -0300

    First commit

---
+++ models/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+
+const Sockets = require('./sockets');
+
+
+
+
+class Server {
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+
+        //http server
+        this.server = http.createServer( this.app );
+
+        //confiuguraciones de sockets
+        this.io = socketio( this.server, {/*configuraciones */} )
+        
+    }
+
+    configurarSockets(){
+        new Sockets( this.io );
+    }
+
+    middlewares(){
+        //deplegar el directorio publico
+        this.app.use( express.static( path.resolve( __dirname, '../public' )));
+    }
+
+    execute(){
+        //inicializar middlewares
+        this.middlewares();
+
+        //inicializar sockets
+        this.configurarSockets();
+
+        //inicializar server
+        this.server.listen(this.port, () => {
+            console.log('Server corriendo en puerto:', this.port);
+        });
+    }
+}
+
+module.exports = Server;
