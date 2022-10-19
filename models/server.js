fd83e5b09fdebad4e26106be900751257dc7b403
Marcia Leite <marcialeite483@gmail.com>
2022-10-19 19:45:13 -0300

    Last changes

--- models/server.js
+++ models/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
+const cors = require('cors');
 
 const Sockets = require('./sockets');
 
@@ -28,6 +29,9 @@ class Server {
     middlewares(){
         //deplegar el directorio publico
         this.app.use( express.static( path.resolve( __dirname, '../public' )));
+
+        //CORS
+        this.app.use( cors() );
     }
 
     execute(){
